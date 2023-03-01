07ba23c9c698ce00af48031a89fba4036a166518
Ferenc Horvath <ferenc.horvath@sportmateclub.com>
2023-03-01 14:57:12 +0100

    seo model meta render fix

--- resources/views/model_meta.blade.php
+++ resources/views/model_meta.blade.php
@@ -1,9 +1,9 @@
-<title>{{ $settings->title }}</title>
-<meta name="description" content="{{ $settings->description }} "/>
-<meta name="keywords" content="{{ $settings->keywords }} "/>
-<meta name="author" content="{{ $settings->author }}">
-<meta name="copyright" content="{{ $settings->copyright }}">
-<meta name="date" content="{{ $settings->date }}">
-<meta name="geo.region" content="{{ $settings->region }}">
-<link rel="canonical" href="{{ $settings->url }}">
-<link rel="home" href="{{ $settings->url }}">
+<title>{{ $settings?->title }}</title>
+<meta name="description" content="{{ $settings?->description }} "/>
+<meta name="keywords" content="{{ $settings?->keywords }} "/>
+<meta name="author" content="{{ $settings?->author }}">
+<meta name="copyright" content="{{ $settings?->copyright }}">
+<meta name="date" content="{{ $settings?->date }}">
+<meta name="geo.region" content="{{ $settings?->region }}">
+<link rel="canonical" href="{{ $settings?->url }}">
+<link rel="home" href="{{ $settings?->url }}">
